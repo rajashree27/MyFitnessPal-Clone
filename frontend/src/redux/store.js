71cafdd238999019f68e593333c64e46c3a976eb
Naushad <17naushad2002@gmail.com>
2022-08-25 23:43:41 +0530

    day-3

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,16 +1,16 @@
-// import {
-// 	legacy_createStore,
-// 	applyMiddleware,
-// 	combineReducers,
-// 	compose,
-// } from "redux";
-// import thunk from "redux-thunk";
+import {
+	legacy_createStore,
+	applyMiddleware,
+	combineReducers,
+	compose,
+} from "redux";
+import thunk from "redux-thunk";
 
-// const rootReducer = combineReducers({});
+const rootReducer = combineReducers({});
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// export const store = legacy_createStore(
-// 	rootReducer,
-// 	composeEnhancers(applyMiddleware(thunk))
-// );
+export const store = legacy_createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk))
+);
