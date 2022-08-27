77c55679fd460f1c82ce4a1d098c8c37f07c282e
rajashree27 <rjchakraborty98@gmail.com>
2022-08-28 01:46:27 +0530

    backend cors updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://myfitnesspal-clone.netlify.app"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
