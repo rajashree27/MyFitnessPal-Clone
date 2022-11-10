5db634179a5b5500111c04a4bd03f0836151e0b2
manishtmtmt <manisht.mt.mt@gmail.com>
2022-11-11 00:32:29 +0530

    fixed backend problems

--- frontend/src/pages/premium/NavbarPremium.jsx
+++ frontend/src/pages/premium/NavbarPremium.jsx
@@ -6,77 +6,74 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const NavbarPremium = () => {
-	const navigate = useNavigate();
-	const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
 
-	const getUser = async () => {
-		try {
-			const url = "https://smooth-flavor-1645.herokuapp.com/login/success";
-			const { data } = await axios.get(url, { withCredentials: true });
-			setUser(data.user._json);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const getUser = async () => {
+    try {
+      const url = "https://myfitnesspalclone17.herokuapp.com/login/success";
+      const { data } = await axios.get(url, { withCredentials: true });
+      setUser(data.user._json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	console.log(user.given_name);
+  console.log(user.given_name);
 
-	const logout = () => {
-		window.open(`https://smooth-flavor-1645.herokuapp.com/logout`, "_self");
-	};
+  const logout = () => {
+    window.open(`https://myfitnesspalclone17.herokuapp.com/logout`, "_self");
+  };
 
-	useEffect(() => {
-		getUser();
-	}, []);
+  useEffect(() => {
+    getUser();
+  }, []);
 
-	return (
-		<Box className="PremiumNavbar">
-			<Box className="navBarTop">
-				<Image src={fitnesLogo} onClick={() => navigate("/")} />
-				{!user.given_name ? (
-					<Box className="loginSignupButtons">
-						<Button
-							variant="ghost"
-							color="#0066EE"
-							onClick={() => navigate("/login")}
-						>
-							Log In
-						</Button>
-						<Divider orientation="vertical" />
-						<Button
-							variant="ghost"
-							color="#0066EE"
-							onClick={() => navigate("/signup")}
-						>
-							Sign Up
-						</Button>
-					</Box>
-				) : (
-					<Box className="loginSignupButtons">
-						<Button
-							variant="ghost"
-							color="#0066EE"
-						>
-							{user.given_name}
-						</Button>
-						<Divider orientation="vertical" />
-						<Button variant="ghost" color="#0066EE" onClick={logout}>
-							Log out
-						</Button>
-					</Box>
-				)}
-			</Box>
-			<Box className="navBarBottom">
-				<Box onClick={() => navigate("/")}>ABOUT</Box>
-				<Box onClick={() => navigate("/foods")}>FOOD</Box>
-				<Box onClick={() => navigate("/")}>EXERCISE</Box>
-				<Box onClick={() => navigate("/apps")}>APPS</Box>
-				<Box>COMMUNITY</Box>
-				<Box onClick={() => navigate("/blog")}>BLOG</Box>
-				<Box onClick={() => navigate("/premium")}>PREMIUM</Box>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box className="PremiumNavbar">
+      <Box className="navBarTop">
+        <Image src={fitnesLogo} onClick={() => navigate("/")} />
+        {!user.given_name ? (
+          <Box className="loginSignupButtons">
+            <Button
+              variant="ghost"
+              color="#0066EE"
+              onClick={() => navigate("/login")}
+            >
+              Log In
+            </Button>
+            <Divider orientation="vertical" />
+            <Button
+              variant="ghost"
+              color="#0066EE"
+              onClick={() => navigate("/signup")}
+            >
+              Sign Up
+            </Button>
+          </Box>
+        ) : (
+          <Box className="loginSignupButtons">
+            <Button variant="ghost" color="#0066EE">
+              {user.given_name}
+            </Button>
+            <Divider orientation="vertical" />
+            <Button variant="ghost" color="#0066EE" onClick={logout}>
+              Log out
+            </Button>
+          </Box>
+        )}
+      </Box>
+      <Box className="navBarBottom">
+        <Box onClick={() => navigate("/")}>ABOUT</Box>
+        <Box onClick={() => navigate("/foods")}>FOOD</Box>
+        <Box onClick={() => navigate("/")}>EXERCISE</Box>
+        <Box onClick={() => navigate("/apps")}>APPS</Box>
+        <Box>COMMUNITY</Box>
+        <Box onClick={() => navigate("/blog")}>BLOG</Box>
+        <Box onClick={() => navigate("/premium")}>PREMIUM</Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default NavbarPremium;
--- frontend/src/pages/signup/SignupCreateAcc.jsx
+++ frontend/src/pages/signup/SignupCreateAcc.jsx
@@ -1,11 +1,4 @@
-import {
-
-	Box,
-	Button,
-	Heading,
-	Input,
-	Text,
-} from "@chakra-ui/react";
+import { Box, Button, Heading, Input, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import SignupNavbar from "../../components/signup/SignupNavbar";
 import "./SignupStyles.css";
@@ -15,139 +8,138 @@ import { useDispatch, useSelector } from "react-redux";
 import { registerAPI } from "../../redux/auth/register/actionsRegister";
 
 const SignupCreateAcc = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	
+  const [regCreds, setRegCreds] = useState({
+    email: "",
+    password: "",
+    username: JSON.parse(localStorage.getItem("username")),
+  });
 
-	const [regCreds, setRegCreds] = useState({
-		email: "",
-		password: "",
-		username: JSON.parse(localStorage.getItem("username"))
-	});
-
-	const handleRegChange = (e) => {
-		const { name, value } = e.target;
-		setRegCreds({
-			...regCreds,
-			[name]: value,
-		});
-	};
-
-	const handleRegFormSubmit = (e) => {
-		e.preventDefault();
-		console.log(regCreds)
-		dispatch(registerAPI(regCreds));
-		// console.log(regCreds);
-	};
-
-	
-	const { isReg } = useSelector((state) => state.register);
-  
-   const googleAuth = () => {
-    window.open("https://smooth-flavor-1645.herokuapp.com/google/callback", "_self");
+  const handleRegChange = (e) => {
+    const { name, value } = e.target;
+    setRegCreds({
+      ...regCreds,
+      [name]: value,
+    });
   };
 
-	useEffect(() => {
-		if (isReg) navigate("/signupCongrats");
-	}, [navigate, isReg]);
+  const handleRegFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(regCreds);
+    dispatch(registerAPI(regCreds));
+    // console.log(regCreds);
+  };
 
-	return (
-		<>
-			<SignupNavbar />
-			<Box className="signupWrapper">
-				<Box className="insideBox" h="fit-content" p="20px">
-					<form action="submit" onSubmit={handleRegFormSubmit}>
-						<Box>
-							<Heading fontSize="20px" my="10px">
-								Almost there! Create your account.
-							</Heading>
+  const { isReg } = useSelector((state) => state.register);
 
-							<Input
-								placeholder="Email Address"
-								h="50px"
-								my="20px"
-								isRequired
-								type="email"
-								name="email"
-								value={regCreds.email}
-								onChange={handleRegChange}
-							/>
-							<Input
-								placeholder="Create a password"
-								h="50px"
-								isRequired
-								type="password"
-								name="password"
-								value={regCreds.password}
-								onChange={handleRegChange}
-							/>
+  const googleAuth = () => {
+    window.open(
+      "https://myfitnesspalclone17.herokuapp.com/google/callback",
+      "_self"
+    );
+  };
 
-							<Text color="#A0A0A0" mb="10px" align="left" mt="5px">
-								Must be at least 10 characters, no spaces.
-							</Text>
-						</Box>
+  useEffect(() => {
+    if (isReg) navigate("/signupCongrats");
+  }, [navigate, isReg]);
 
-						<Box mt="40px">
-							<Text color="#A0A0A0" align="left" mt="5px">
-								By signing up for MyFitnessPal, you are agreeing to our
-							</Text>
-							<Text
-								color="blue"
-								mb="10px"
-								align="center"
-								style={{ cursor: "pointer" }}
-							>
-								Privacy Policy <span style={{ color: "#A0A0A0" }}>and</span>{" "}
-								Terms.
-							</Text>
-							<Button
-								bg="#0066EE"
-								color="white"
-								w="350px"
-								fontSize="18px"
-								mt="10px"
-								// onClick={handleCreateAccountClick}
-								type="submit"
-							>
-								CONTINUE
-							</Button>
+  return (
+    <>
+      <SignupNavbar />
+      <Box className="signupWrapper">
+        <Box className="insideBox" h="fit-content" p="20px">
+          <form action="submit" onSubmit={handleRegFormSubmit}>
+            <Box>
+              <Heading fontSize="20px" my="10px">
+                Almost there! Create your account.
+              </Heading>
 
-							<Text color="#A0A0A0" mb="10px" align="center">
-								or
-							</Text>
-							<Button
-								leftIcon={<FcGoogle />}
-								colorScheme="blue"
-								variant="outline"
-								w="350px"
-								color="#0066EE"
+              <Input
+                placeholder="Email Address"
+                h="50px"
+                my="20px"
+                isRequired
+                type="email"
+                name="email"
+                value={regCreds.email}
+                onChange={handleRegChange}
+              />
+              <Input
+                placeholder="Create a password"
+                h="50px"
+                isRequired
+                type="password"
+                name="password"
+                value={regCreds.password}
+                onChange={handleRegChange}
+              />
+
+              <Text color="#A0A0A0" mb="10px" align="left" mt="5px">
+                Must be at least 10 characters, no spaces.
+              </Text>
+            </Box>
+
+            <Box mt="40px">
+              <Text color="#A0A0A0" align="left" mt="5px">
+                By signing up for MyFitnessPal, you are agreeing to our
+              </Text>
+              <Text
+                color="blue"
+                mb="10px"
+                align="center"
+                style={{ cursor: "pointer" }}
+              >
+                Privacy Policy <span style={{ color: "#A0A0A0" }}>and</span>{" "}
+                Terms.
+              </Text>
+              <Button
+                bg="#0066EE"
+                color="white"
+                w="350px"
+                fontSize="18px"
+                mt="10px"
+                // onClick={handleCreateAccountClick}
+                type="submit"
+              >
+                CONTINUE
+              </Button>
+
+              <Text color="#A0A0A0" mb="10px" align="center">
+                or
+              </Text>
+              <Button
+                leftIcon={<FcGoogle />}
+                colorScheme="blue"
+                variant="outline"
+                w="350px"
+                color="#0066EE"
                 onClick={googleAuth}
-							>
-								CONTINUE WITH GOOGLE
-							</Button>
+              >
+                CONTINUE WITH GOOGLE
+              </Button>
 
-							<Text color="#A0A0A0" my="10px" align="center" fontSize="sm">
-								We will never post anything without your permission
-							</Text>
-							<Text color="#A0A0A0" align="center" fontSize="xs">
-								This site is protected by reCAPTCHA and the Google{" "}
-								<span style={{ color: "blue", cursor: "pointer" }}>
-									Privacy Policy
-								</span>
-							</Text>
-							<Text color="#A0A0A0" mb="10px" align="center" fontSize="xs">
-								and{" "}
-								<span style={{ color: "blue", cursor: "pointer" }}>Terms</span>{" "}
-								apply
-							</Text>
-						</Box>
-					</form>
-				</Box>
-			</Box>
-		</>
-	);
-
+              <Text color="#A0A0A0" my="10px" align="center" fontSize="sm">
+                We will never post anything without your permission
+              </Text>
+              <Text color="#A0A0A0" align="center" fontSize="xs">
+                This site is protected by reCAPTCHA and the Google{" "}
+                <span style={{ color: "blue", cursor: "pointer" }}>
+                  Privacy Policy
+                </span>
+              </Text>
+              <Text color="#A0A0A0" mb="10px" align="center" fontSize="xs">
+                and{" "}
+                <span style={{ color: "blue", cursor: "pointer" }}>Terms</span>{" "}
+                apply
+              </Text>
+            </Box>
+          </form>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default SignupCreateAcc;
